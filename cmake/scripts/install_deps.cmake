2b552430d57c247d526394cf0075ebb7a680d725
Artem Pikulin <ortgma@gmail.com>
2018-04-02 13:44:34 +0700

    Don't download and install leveldb.

--- cmake/scripts/install_deps.cmake
+++ cmake/scripts/install_deps.cmake
@@ -6,6 +6,3 @@ set(SERVER "https://github.com/ethereum/cpp-dependencies/releases/download/vc140
 function(download_and_install PACKAGE_NAME)
     download_and_unpack("${SERVER}${PACKAGE_NAME}.tar.gz" ${INSTALL_DIR})
 endfunction(download_and_install)
-
-
-download_and_install("leveldb-1.2")
